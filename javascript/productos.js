93bb87195961f850a5d8ac3f9079d6308b5aeba7
LorenzoGio <142460492+LorenzoGio@users.noreply.github.com>
2024-01-10 21:44:51 -0300

    Add files via upload

---
+++ javascript/productos.js
@@ -0,0 +1,31 @@
+const productos = [{
+    id: 1,
+    foto: "https://http2.mlstatic.com/D_NQ_NP_614833-MLU71065453080_082023-O.webp",
+    nombre: "Masajeador Roller Jade",
+    precio: 4500
+},
+{
+    id: 2,
+    foto: "https://http2.mlstatic.com/D_NQ_NP_818874-MLA48072974596_102021-O.webp",
+    nombre: "Masajeador Manual Corporal",
+    precio: 5900
+},
+{
+    id: 3,
+    foto: "https://http2.mlstatic.com/D_NQ_NP_855040-MLA53786425700_022023-O.webp",
+    nombre: "Masajeador Capilar Cabeza",
+    precio: 300
+},
+{
+    id: 4,
+    foto: "https://http2.mlstatic.com/D_NQ_NP_711518-MLA71177443537_082023-O.webp",
+    nombre: "Masajeador Manual Rodillo De Madera",
+    precio: 3500
+},
+{
+    id: 5,
+    foto: "https://http2.mlstatic.com/D_NQ_NP_797057-MLU71662688604_092023-O.webp",
+    nombre: "Masajeador Cuello Cervical",
+    precio: 16000
+}
+];
